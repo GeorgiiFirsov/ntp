f5daba45390bd0b4a6a649de6ea46ca27ca414b6
Georgy Firsov <gfirsov007@gmail.com>
2022-12-05 12:45:00 +0300

    feat(ntp): dependency from ATL removed

--- ntp/include/details/utils.hpp
+++ ntp/include/details/utils.hpp
@@ -185,4 +185,75 @@ private:
     RTL_RESOURCE resource_;
 };
 
+
+/**
+ * @brief Wrapper for Win32 API Event handle. Automatically closes HANDLE in destructor.
+ */
+class Event final
+{
+    Event(const Event&)            = delete;
+    Event& operator=(const Event&) = delete;
+
+public:
+    /**
+     * @brief The most generic constructor, accepts all of the arguments, that CreateEvent accepts.
+     *
+     * @param security_attributes A pointer to a SECURITY_ATTRIBUTES structure. If this parameter is NULL,
+     *                            the handle cannot be inherited by child processes.
+     * @param manual_reset        If this parameter is TRUE, the function creates a manual-reset event object,
+     *                            which requires the use of the ResetEvent function to set the event state to
+     *                            nonsignaled. If this parameter is FALSE, the function creates an auto-reset
+     *                            event object, and system automatically resets the event state to nonsignaled
+     *                            after a single waiting thread has been released.
+     * @param initially_signaled  If this parameter is TRUE, the initial state of the event object is signaled;
+     *                            otherwise, it is nonsignaled.
+     * @param name                The name of the event object. The name is limited to MAX_PATH characters.
+     *                            Name comparison is case sensitive.
+     */
+    explicit Event(LPSECURITY_ATTRIBUTES security_attributes, BOOL manual_reset, BOOL initially_signaled, LPCWSTR name = nullptr);
+
+    /**
+     * @brief Constructur, that accepts all CreateEvent arguments, but without the very first one. Sets it to nullptr.
+     *
+     * @param manual_reset       If this parameter is TRUE, the function creates a manual-reset event object,
+     *                           which requires the use of the ResetEvent function to set the event state to
+     *                           nonsignaled. If this parameter is FALSE, the function creates an auto-reset
+     *                           event object, and system automatically resets the event state to nonsignaled
+     *                           after a single waiting thread has been released.
+     * @param initially_signaled If this parameter is TRUE, the initial state of the event object is signaled;
+     *                           otherwise, it is nonsignaled.
+     * @param name               The name of the event object. The name is limited to MAX_PATH characters.
+     *                           Name comparison is case sensitive.
+     */
+    explicit Event(BOOL manual_reset, BOOL initially_signaled, LPCWSTR name = nullptr);
+
+    /**
+     * @brief Destructor closes opened event handle.
+     */
+    ~Event();
+
+    /**
+     * @brief Implicit cats to HANDLE (useful for more readable code)
+     */
+    operator HANDLE() const noexcept { return event_; }
+
+    /**
+     * @brief Wrapper for SetEvent.
+     *
+     * @throws ntp::exception::Win32Exception in case of failure.
+     */
+    void Set();
+
+    /**
+     * @brief Wrapper for ResetEvent()
+     *
+     * @throws ntp::exception::Win32Exception in case of failure.
+     */
+    void Reset();
+
+private:
+    // Internal event handle
+    HANDLE event_;
+};
+
 }  // namespace ntp::details
